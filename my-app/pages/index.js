0a11005acb548438a93df27c72fa3efd6ec3cbd1
adriancr1 <adrian.casal.rodriguez@gmail.com>
2022-09-19 11:25:59 +0200

    fixed Web3Provider

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -284,7 +284,7 @@ export default function Home() {
     // Connect to Metamask
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.web3Provider(provider);
+    const web3Provider = new providers.Web3Provider(provider);
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
